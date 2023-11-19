2bbf8132a07cf09f2d4fe9fd54db53ef932b97af
Prakash Yogi <prakash.y@fyle.in>
2023-11-19 21:50:37 +0530

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const morgan = require('morgan')
 const logController = require("./controllers/logController");
+const cors = require("cors");
 const app = express();
 
-
+app.use(cors());
 app.use(morgan('combined'))
 app.use(express.json());
 app.use((req, res, next) => {
